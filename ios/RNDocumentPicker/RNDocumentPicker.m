4411c98bc884a42e8b64a8697722ce1f00b01d8b
man.leung@zensis.com <man.leung@zensis.com>
2017-08-18 19:02:28 +0800

    Update import document logic Add clear temp files function Update readme

--- ios/RNDocumentPicker/RNDocumentPicker.m
+++ ios/RNDocumentPicker/RNDocumentPicker.m
@@ -8,8 +8,12 @@
 #import "RCTBridge.h"
 #endif
 
+#import <Foundation/Foundation.h>
+
 #define IDIOM    UI_USER_INTERFACE_IDIOM()
 #define IPAD     UIUserInterfaceIdiomPad
+
+#define TEMP_FOLDER @"react-native-document-picker/"
 
 @interface RNDocumentPicker () <UIDocumentMenuDelegate,UIDocumentPickerDelegate>
 @end
@@ -40,28 +44,28 @@ RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(show:(NSDictionary *)options
                   callback:(RCTResponseSenderBlock)callback) {
-
+    
     NSArray *allowedUTIs = [RCTConvert NSArray:options[@"filetype"]];
     UIDocumentMenuViewController *documentPicker = [[UIDocumentMenuViewController alloc] initWithDocumentTypes:(NSArray *)allowedUTIs inMode:UIDocumentPickerModeImport];
-
+    
     [composeCallbacks addObject:callback];
-
-
+    
+    
     documentPicker.delegate = self;
     documentPicker.modalPresentationStyle = UIModalPresentationFormSheet;
-
+    
     UIViewController *rootViewController = [[[[UIApplication sharedApplication]delegate] window] rootViewController];
     while (rootViewController.modalViewController) {
         rootViewController = rootViewController.modalViewController;
     }
-
+    
     if ( IDIOM == IPAD ) {
         NSNumber *top = [RCTConvert NSNumber:options[@"top"]];
         NSNumber *left = [RCTConvert NSNumber:options[@"left"]];
         [documentPicker.popoverPresentationController setSourceRect: CGRectMake([left floatValue], [top floatValue], 0, 0)];
         [documentPicker.popoverPresentationController setSourceView: rootViewController.view];
     }
-
+    
     [rootViewController presentViewController:documentPicker animated:YES completion:nil];
 }
 
@@ -69,7 +73,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)options
 - (void)documentMenu:(UIDocumentMenuViewController *)documentMenu didPickDocumentPicker:(UIDocumentPickerViewController *)documentPicker {
     documentPicker.delegate = self;
     documentPicker.modalPresentationStyle = UIModalPresentationFormSheet;
-
+    
     UIViewController *rootViewController = [[[[UIApplication sharedApplication]delegate] window] rootViewController];
     
     while (rootViewController.modalViewController) {
@@ -79,7 +83,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)options
         [documentPicker.popoverPresentationController setSourceRect: CGRectMake(rootViewController.view.frame.size.width/2, rootViewController.view.frame.size.height - rootViewController.view.frame.size.height / 6, 0, 0)];
         [documentPicker.popoverPresentationController setSourceView: rootViewController.view];
     }
-
+    
     [rootViewController presentViewController:documentPicker animated:YES completion:nil];
 }
 
@@ -87,30 +91,51 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)options
     if (controller.documentPickerMode == UIDocumentPickerModeImport) {
         RCTResponseSenderBlock callback = [composeCallbacks lastObject];
         [composeCallbacks removeLastObject];
-
-        [url startAccessingSecurityScopedResource];
-
-        NSFileCoordinator *coordinator = [[NSFileCoordinator alloc] init];
-        __block NSError *error;
-
-        [coordinator coordinateReadingItemAtURL:url options:NSFileCoordinatorReadingResolvesSymbolicLink error:&error byAccessor:^(NSURL *newURL) {
+        
+        NSError *error = nil;
+        
+        NSString *tmpFolder = TEMP_FOLDER;
+        NSString *tmpFullPath = [NSTemporaryDirectory() stringByAppendingString:tmpFolder];
+        NSString *tmpFilePath = [tmpFullPath stringByAppendingPathComponent:[url lastPathComponent]];
+        NSURL *tempFile = [NSURL fileURLWithPath:tmpFilePath];
+        
+        if(![[NSFileManager defaultManager] fileExistsAtPath:tmpFullPath])
+            [[NSFileManager defaultManager] createDirectoryAtPath:tmpFullPath withIntermediateDirectories:YES attributes:nil error:&error]; //Create folder
+        else {
+            if([[NSFileManager defaultManager] fileExistsAtPath:tmpFilePath])
+                [[NSFileManager defaultManager]  removeItemAtPath:tmpFilePath error:&error];
+        }
+        [[NSFileManager defaultManager] copyItemAtURL:url toURL:tempFile error:&error];
+        
+        if(error == nil) {
             NSMutableDictionary* result = [NSMutableDictionary dictionary];
-
-            [result setValue:newURL.absoluteString forKey:@"uri"];
-            [result setValue:[newURL lastPathComponent] forKey:@"fileName"];
-
+            [result setValue:tempFile.absoluteString forKey:@"uri"];
+            [result setValue:[tempFile lastPathComponent] forKey:@"fileName"];
             NSError *attributesError = nil;
-            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:newURL.path error:&attributesError];
+            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:tempFile.path error:&attributesError];
             if(!attributesError) {
                 [result setValue:[fileAttributes objectForKey:NSFileSize] forKey:@"fileSize"];
             } else {
                 NSLog(@"%@", attributesError);
             }
-
             callback(@[[NSNull null], result]);
-        }];
+        }
+        else
+            callback(@[@"Copy failed", [NSNull null]]);
+    }
+}
 
-        [url stopAccessingSecurityScopedResource];
+
+RCT_EXPORT_METHOD(clearTempFiles) {
+    NSString *tmpFolder = TEMP_FOLDER;
+    NSString *tmpFullPath = [NSTemporaryDirectory() stringByAppendingString:tmpFolder];
+    
+    if([[NSFileManager defaultManager] fileExistsAtPath:tmpFullPath]){
+        NSError *error = nil;
+        for (NSString *file in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:tmpFullPath error:&error])
+        {
+            [[NSFileManager defaultManager] removeItemAtPath:[NSString stringWithFormat:@"%@%@", tmpFullPath, file] error:&error];
+        }
     }
 }
 
